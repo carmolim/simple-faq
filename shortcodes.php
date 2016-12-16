96745a9b8c795c4da420ed30d2d265277e3641fb
Augusto Carmo <carmolim@gmail.com>
2016-12-16 09:50:11 -0200

    two new short codes, one to display only the text input and the buttons for each category and the second one just to display the questions and answers

--- shortcodes.php
+++ shortcodes.php
@@ -1,13 +1,72 @@
 <?php
 
+	function sfaq_filters($atts, $content=null){
+
+		echo '<div class="sfaq-filters">';
+		echo '	<input type="text" id="quicksearch" placeholder="Search">';
+		echo '	<div id="filters" class="button-group">';
+		echo '		<button class="button is-checked" data-filter="all">Todos</button>';
+
+		if ( ! empty( $terms ) ) {
+
+			foreach( $terms as $term ) {
+				echo '		<button class="button" data-filter="' . $term->slug .'">' . $term->name .'</button>';
+			}
+		}
+
+		echo '	</div>';
+		echo '</div>';
+	}
+
+	add_shortcode('sfaq_filters', 'sfaq_filters');
+
+
+	function sfaq_questions($atts, $content=null){
+
+		// WP_Query arguments
+		$args = array(
+			'post_type'              => SFAQ_CTP,
+			'order'                  => 'ASC',
+			'orderby'                => 'title',
+		);
+
+		// The Query
+		$the_query = new WP_Query( $args );
+
+		$terms = get_terms( array(
+			'taxonomy' => SFAQ_CTP . '_taxonomy',
+		) );
+
+		echo '<div class="b-questions">';
+
+		// The Loop
+		if ( $the_query->have_posts() ) {
+
+			while ( $the_query->have_posts() ) {
+				$the_query->the_post();
+				include 'question-block.php';
+			}
+
+			/* Restore original Post Data */
+			wp_reset_postdata();
+
+		} else {
+			// no posts found
+		}
+
+		echo '</div>';
+	}
+
+	add_shortcode('sfaq_questions', 'sfaq_questions');
+
 	function sfaq_list_all_questions($atts, $content=null){
 
 		// WP_Query arguments
 		$args = array(
-		'post_type'              => SFAQ_CTP,
-		'order'                  => 'ASC',
-		'orderby'                => 'title',
-	);
+			'post_type'              => SFAQ_CTP,
+			'order'                  => 'ASC',
+			'orderby'                => 'title',
+		);
 
 		// The Query
 		$the_query = new WP_Query( $args );
