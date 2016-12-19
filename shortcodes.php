63a9242dc7bd9b152c9b76d3bf7c9282e527a432
Augusto Carmo <carmolim@gmail.com>
2016-12-19 10:56:14 -0200

    adding filters for better customisation, better class names following BEM pattern

--- shortcodes.php
+++ shortcodes.php
@@ -2,26 +2,36 @@
 
 	function sfaq_filters($atts, $content=null){
 
-		echo '<div class="sfaq-filters">';
-		echo '	<input type="text" id="quicksearch" placeholder="Search">';
-		echo '	<div id="filters" class="button-group">';
-		echo '		<button class="button is-checked" data-filter="all">Todos</button>';
+		$terms = get_terms( array(
+    		'taxonomy' => SFAQ_CTP . '_taxonomy',
+		) );
+
+		$html = '';
+
+		$html .= '<div class="sfaq-filters">';
+		$html .= '	<input class="sfaq-filters__input" type="text" id="quicksearch" placeholder="Search">';
+		$html .= '	<div id="filters" class="sfaq-filters__button-group">';
+		$html .= '		<button class="sfaq-filters__button is-checked" data-filter="all">Todos</button>';
 
 		if ( ! empty( $terms ) ) {
 
 			foreach( $terms as $term ) {
-				echo '		<button class="button" data-filter="' . $term->slug .'">' . $term->name .'</button>';
+				$html .= '		<button class="sfaq-filters__button" data-filter="' . $term->slug .'">' . $term->name .'</button>';
 			}
 		}
 
-		echo '	</div>';
-		echo '</div>';
+		$html .= '	</div>';
+		$html .= '</div>';
+
+		return apply_filters( 'sfaq_filters_html', $html );
 	}
 
 	add_shortcode('sfaq_filters', 'sfaq_filters');
 
 
 	function sfaq_questions($atts, $content=null){
+
+		$html = '';
 
 		// WP_Query arguments
 		$args = array(
@@ -37,14 +47,16 @@
 			'taxonomy' => SFAQ_CTP . '_taxonomy',
 		) );
 
-		echo '<div class="b-questions">';
+		$html .= '<div class="b-questions">';
 
 		// The Loop
 		if ( $the_query->have_posts() ) {
 
 			while ( $the_query->have_posts() ) {
 				$the_query->the_post();
+				ob_start();
 				include 'question-block.php';
+				$html .= ob_get_clean();
 			}
 
 			/* Restore original Post Data */
@@ -54,7 +66,9 @@
 			// no posts found
 		}
 
-		echo '</div>';
+		$html .= '</div>';
+
+		return apply_filters( 'sfaq_questions_html', $html );
 	}
 
 	add_shortcode('sfaq_questions', 'sfaq_questions');
