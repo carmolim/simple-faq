07c6083c2daec51363858c92f19ba35b4ee11610
Augusto Carmo <augusto@massimo.studio>
2017-03-29 20:23:08 -0300

    new short code just for the filters

--- shortcodes.php
+++ shortcodes.php
@@ -1,13 +1,87 @@
 <?php
 
+	function sfaq_filters($atts, $content=null){
+
+		$terms = get_terms( array(
+    		'taxonomy' => SFAQ_CTP . '_taxonomy',
+		) );
+
+		$html = '';
+
+		$html .= '<div class="sfaq-filters">';
+		$html .= '	<input class="sfaq-filters__input" type="text" id="quicksearch" placeholder="O que você está procurando?">';
+		$html .= '	<div id="filters" class="sfaq-filters__button-group">';
+		$html .= '		<button class="sfaq-filters__button is-checked" data-filter="all">Todos</button>';
+
+		if ( ! empty( $terms ) ) {
+
+			foreach( $terms as $term ) {
+				$html .= '		<button class="sfaq-filters__button" data-filter="' . $term->slug .'">' . $term->name .'</button>';
+			}
+		}
+
+		$html .= '	</div>';
+		$html .= '</div>';
+
+		return apply_filters( 'sfaq_filters_html', $html );
+	}
+
+	add_shortcode('sfaq_filters', 'sfaq_filters');
+
+
+	function sfaq_questions($atts, $content=null){
+
+		$html = '';
+
+		// WP_Query arguments
+		$args = array(
+			'post_type'      => SFAQ_CTP,
+			'order'          => 'ASC',
+			'orderby'        => 'date',
+			'posts_per_page' => -1
+		);
+
+		// The Query
+		$the_query = new WP_Query( $args );
+
+		$terms = get_terms( array(
+			'taxonomy' => SFAQ_CTP . '_taxonomy',
+		) );
+
+		$html .= '<div class="b-questions">';
+
+		// The Loop
+		if ( $the_query->have_posts() ) {
+
+			while ( $the_query->have_posts() ) {
+				$the_query->the_post();
+				ob_start();
+				include 'question-block.php';
+				$html .= ob_get_clean();
+			}
+
+			/* Restore original Post Data */
+			wp_reset_postdata();
+
+		} else {
+			// no posts found
+		}
+
+		$html .= '</div>';
+
+		return apply_filters( 'sfaq_questions_html', $html );
+	}
+
+	add_shortcode('sfaq_questions', 'sfaq_questions');
+
 	function sfaq_list_all_questions($atts, $content=null){
 
 		// WP_Query arguments
 		$args = array(
-		'post_type'              => SFAQ_CTP,
-		'order'                  => 'ASC',
-		'orderby'                => 'title',
-	);
+			'post_type'              => SFAQ_CTP,
+			'order'                  => 'ASC',
+			'orderby'                => 'title',
+		);
 
 		// The Query
 		$the_query = new WP_Query( $args );
